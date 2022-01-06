e39aafc7f4c82954ccc9dfd7228083a052854d31
Mayuresh <mayureshg08@gmail.com>
2022-01-06 12:58:38 +0530

    new programme added

---
+++ Chapter2/12. Strings are Immutable.py
@@ -0,0 +1,7 @@
+string= 'Mayuresh'
+
+print(string.replace('a','A'))           #Strings are immutable
+print(string)
+
+new_string= string.replace('a','A')      #for change we have to create a new variable.
+print (new_string)
